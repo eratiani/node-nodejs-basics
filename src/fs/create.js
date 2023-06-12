340529a2116ac52b72c5fbc9de516a951b8db0e1
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-13 01:56:57 +0400

    add create method

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import {writeFile, access} from 'node:fs/promises';
+import path from 'path';
+import { join } from 'node:path';
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const create = async () => {
+    const dirPath = join(__dirname, "files");
+    const filePath = join(dirPath,"fresh.txt");
+    const textContent = "I am fresh and young";
     // Write your code here 
+        try {
+            await access(filePath.substring(3)).then(()=>{
+                throw new Error(`FS operation failed`); 
+            })
+        } catch (error) {
+            if (error.message !==`FS operation failed` ) {
+                
+                writeFile(filePath.substring(3), textContent);
+            }else {
+                throw new Error(error.message)
+            }
+        }
+
+        
+   
 };
 
 await create();
