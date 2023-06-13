3a56794c35046dd0e37b7b4c8647b5790f0670a9
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-13 15:14:27 +0400

    add copy method, fix create method

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,4 +1,26 @@
+import {  access, cp } from "node:fs/promises";
+import {dirname} from 'path';
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const copy = async () => {
+    const dirPath = join(__dirname, "files");
+    const targetPath = join(__dirname,"files_copy");
+    try {
+        await access(targetPath).then(()=>{
+            throw new Error("FS operation failed")
+        })
+    } catch (error) {
+        if (error.message !=="FS operation failed") {
+            cp(dirPath,targetPath,{ recursive: true })
+            
+        } else {
+            throw new Error(error.message)
+        }
+    }
+    
     // Write your code here 
 };
 
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,20 +1,23 @@
 import {writeFile, access} from 'node:fs/promises';
-import path from 'path';
+import {dirname} from 'path';
 import { join } from 'node:path';
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const create = async () => {
     const dirPath = join(__dirname, "files");
     const filePath = join(dirPath,"fresh.txt");
     const textContent = "I am fresh and young";
     // Write your code here 
         try {
-            await access(filePath.substring(3)).then(()=>{
+            await access(filePath).then(()=>{
                 throw new Error(`FS operation failed`); 
             })
         } catch (error) {
             if (error.message !==`FS operation failed` ) {
                 
-                writeFile(filePath.substring(3), textContent);
+                writeFile(filePath, textContent);
             }else {
                 throw new Error(error.message)
             }
