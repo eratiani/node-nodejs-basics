e154127888c34086eb119c43b1e117f9dc51c3dc
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-14 00:51:53 +0400

    add all fs methods

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,4 +1,25 @@
+import {  access} from "node:fs/promises";
+import {dirname} from 'path';
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { readdir } from "node:fs";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const list = async () => {
+    try {
+        const dirPath = join(__dirname, "files")
+        access(dirPath).then(()=>{
+            readdir(dirPath,(err,fileNames)=>{
+                fileNames.forEach(file => {
+                    console.log(file);
+                  });
+            })
+        }).catch((err)=>{
+            throw new Error("FS operation failed")
+        })
+    } catch (error) {
+        throw new Error(error.message)
+    }
     // Write your code here 
 };
 
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,22 @@
+import {  access, readFile} from "node:fs/promises";
+import {dirname} from 'path';
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const read = async () => {
+    try {
+        const file = join(__dirname,"files/fileToRead.txt");
+        access(file).then(()=>{
+            readFile(file).then((res)=>{
+                console.log(res.toString());
+            })
+        }).catch((err)=>{
+            throw new Error("FS operation failed")
+        })
+    } catch (error) {
+        throw new Error(error.message)
+    }
     // Write your code here 
 };
 
