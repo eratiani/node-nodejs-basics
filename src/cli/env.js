89f7eddaa6b79ed352cf1dbf779bbbaaefb27daa
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-14 16:17:10 +0400

    add cli methods

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  for (const env in process.env) {
+    if (variable.startsWith(prefix)) {
+      const value = process.env[variable];
+      console.log(`${variable} = ${value}`);
+    }
+  }
+  // Write your code here
 };
 
 parseEnv();
