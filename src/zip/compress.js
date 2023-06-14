22a27560c5c9fffab65a6a53c201290d120cac77
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-15 00:22:38 +0400

    add wt-zip methods

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,4 +1,14 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "node:url";
+import { createReadStream, createWriteStream } from "fs"; 
+import { createGzip } from "zlib";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const compress = async () => {
+    const readStream = createReadStream(join(__dirname, "files/fileToCompress.txt"));
+    const writeStream = createWriteStream(join(__dirname,"files/archive.gz"));
+    const gzip = createGzip();
+    readStream.pipe(gzip).pipe(writeStream);
     // Write your code here 
 };
 
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,4 +1,14 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "node:url";
+import { createReadStream, createWriteStream } from "fs"; 
+import { createGunzip } from "zlib";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const decompress = async () => {
+    const writeStream = createWriteStream(join(__dirname, "files/fileToCompress.txt"));
+    const readStream = createReadStream(join(__dirname,"files/archive.gz"));
+    const gzip = createGunzip();
+    readStream.pipe(gzip).pipe(writeStream);
     // Write your code here 
 };
 
