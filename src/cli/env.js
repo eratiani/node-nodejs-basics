b37b8f4c775637f8224abb97914b4129f790227b
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-14 19:38:43 +0400

    add streams methods

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,6 @@
 const parseEnv = () => {
   const prefix = "RSS_";
-  for (const env in process.env) {
+  for (const variable in process.env) {
     if (variable.startsWith(prefix)) {
       const value = process.env[variable];
       console.log(`${variable} = ${value}`);
