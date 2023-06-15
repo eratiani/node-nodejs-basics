24aac8849b6c255ccf9c1d394f595ee5ae2fcd20
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-15 09:45:58 +0400

    refactor code finish project

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -5,12 +5,12 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const spawnChildProcess = async (args) => {
-    const filePath = join(__dirname,"files/script.js") 
-    const childProcess = spawn("node",[filePath, ...args])
-    process.stdin.pipe(childProcess.stdin);
-    childProcess.stdout.pipe(process.stdout);
-    // Write your code here
+  const filePath = join(__dirname, "files/script.js");
+  const childProcess = spawn("node", [filePath, ...args]);
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+  // Write your code here
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess(  ["someArgument1", "someArgument2"] );
+spawnChildProcess(["someArgument1", "someArgument2"]);
